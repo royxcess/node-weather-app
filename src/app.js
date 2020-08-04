1289185183d6503fb1b5931355bc9944acfd3fbc
Shantanu Roy <theroyshantanu@gmail.com>
2020-08-04 20:02:28 +0530

    Creating Heroku app

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const chalk = require('chalk')
 
 //craeting Express application
 const app = express()
+const port = process.env.PORT || 3000
 
 //Define path for express config
 const publicDirPath = path.join(__dirname, '../public')
@@ -107,6 +108,6 @@ app.get('*', (req, res)=> {
     })
 })
 
-app.listen(3000, ()=> {
-    console.log('Server is up & running on port 3000')
+app.listen(port, ()=> {
+    console.log('Server is up & running on port ' + port)
 })
